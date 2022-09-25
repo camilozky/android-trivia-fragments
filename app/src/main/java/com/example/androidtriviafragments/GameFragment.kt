c4d4545bb9707433b2f33a70edda475027ae6ed2
Milo <camilo.iush@gmail.com>
2022-09-24 23:03:05 -0400

    Conditional-Navigation

--- app/src/main/java/com/example/androidtriviafragments/GameFragment.kt
+++ app/src/main/java/com/example/androidtriviafragments/GameFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.androidtriviafragments.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -90,9 +91,12 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
+                        view.findNavController()
+                            .navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
+                    view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment2)
                 }
             }
         }
